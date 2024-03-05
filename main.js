b5a1e11306c94496ad80e9eff6e9e96760b8047c
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-05 11:58:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Declare a boolean variable
+var isTrue = true;
+var isFalse = false;
+// You can also use the 'let' keyword without specifying the type explicitly
+var isActive = false; // TypeScript infers the type as boolean
+// Example of using boolean variables in a conditional statement
+if (isActive) {
+    console.log("The condition is true");
+}
+else {
+    console.log("The condition is false");
+}
---
+++ main.ts
@@ -0,0 +1,13 @@
+// Declare a boolean variable
+let isTrue: boolean = true;
+let isFalse: boolean = false;
+
+// You can also use the 'let' keyword without specifying the type explicitly
+let isActive = false; // TypeScript infers the type as boolean
+
+// Example of using boolean variables in a conditional statement
+if (isActive) {
+    console.log("The condition is true");
+} else {
+    console.log("The condition is false");
+}
